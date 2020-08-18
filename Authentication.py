4228f3272a5930c4be5ea4369d1f39341b1793e0
GyftakisK <gyftakiskon@gmail.com>
2020-08-18 19:52:31 +0300

    Medknow: Fix ticket expiration issue

--- Authentication.py
+++ Authentication.py
@@ -3,6 +3,7 @@
 # See https://documentation.uts.nlm.nih.gov/rest/authentication.html for full explanation
 
 import requests
+import datetime
 from pyquery import PyQuery as pq
 
 uri = "https://utslogin.nlm.nih.gov"
@@ -25,7 +26,8 @@ class Authentication(metaclass=Singleton):
         # self.password=password
         self.apikey = apikey
         self.service = "http://umlsks.nlm.nih.gov"
-        self._tgt = self._gettgt()
+        self._tgt = None
+        self._tgt_exp_time = None
 
     def _gettgt(self):
         # params = {'username': self.username,'password': self.password}
@@ -36,15 +38,19 @@ class Authentication(metaclass=Singleton):
         # extract the entire URL needed from the HTML form (action attribute) returned 
         # looks similar to https://utslogin.nlm.nih.gov/cas/v1/tickets/TGT-36471-aYqNLN2rFIJPXKzxwdTNC5ZT7z3B3cTAKfSc5ndHQcUxeaDOLN-cas
         # we make a POST call to this URL in the getst method
-        tgt = d.find('form').attr('action')
+        self._tgt = d.find('form').attr('action')
+        self._tgt_exp_time = datetime.datetime.now() +  datetime.timedelta(hours=7)
         print("Got ticket")
-        return tgt
 
     def getst(self):
-
+        if not self._tgt or self._has_tgt_expired():
+            self._gettgt()
         params = {'service': self.service}
         h = {"Content-type": "application/x-www-form-urlencoded", 
              "Accept": "text/plain", "User-Agent": "python"}
         r = requests.post(self._tgt, data=params, headers=h)
         st = r.text
         return st
+
+    def _has_tgt_expired(self):
+        return datetime.datetime.now() > self._tgt_exp_time
